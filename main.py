dfd30650cdc99f63570245d4d9a3235676a9e164
Alexey <alexcheremisin@mail.ru>
2023-05-19 20:34:01 +0300

    Start

---
+++ main.py
@@ -0,0 +1,22 @@
+# a = int(input("Your age?"))
+# if a < 12:
+#     print("child")
+# elif a < 45:
+#     print("midman")
+# else:
+#     print("oldman")
+# import math
+# n = int(input("speed"))
+# m = int(input("km"))
+# res = m // n + int(m % n > 0)
+
+# print(res)
+
+a = int(input("First"))
+b = int(input("Second"))
+c = int(input("Third"))
+
+sum = a + b + c
+res = sum // 2 + int(sum % 2 >0 )
+
+print(res)
